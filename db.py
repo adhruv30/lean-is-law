fc29b2720d61f12bde9d6f7a9635914c8e4e1c8c
Arjun Dhruv <arjundhruv@mac.lan>
2026-06-23 23:28:10 -0700

    database connected as function with un-hardcoded macros

--- db.py
+++ db.py
@@ -6,12 +6,12 @@ load_dotenv()
 name = os.getenv("DB_NAME")
 host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
-connection = psycopg2.connect(dbname=name, host=host, user=user)
-##print("connected successfully")
-cursor = connection.cursor()
-sql = "INSERT INTO food_log (date, food_name, protein, carbs, fat, calories, serving_size) VALUES ('2026-06-22', 'chicken breast', 20.4, 6.7, 3.2, 130, 4.1)"
-cursor.execute(sql)
-connection.commit()
-cursor.execute("SELECT * FROM food_log")
-rows = cursor.fetchall()
-print(rows)
+
+def insert_food_log (date, food_name, protein, carbs, fat, calories, serving_size):
+    connection = psycopg2.connect(dbname=name, host=host, user=user)
+    cursor = connection.cursor()
+    sql = "INSERT INTO food_log (date, food_name, protein, carbs, fat, calories, serving_size) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql, (date, food_name, protein, carbs, fat, calories, serving_size))
+    connection.commit()
+
+
--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import requests
+from db import insert_food_log
 
 #Calling API
 url = "https://api.nal.usda.gov/fdc/v1/foods/search"
@@ -16,7 +17,20 @@ else:
 ##debug that the numbers were actually formatted as strings
 # print(data["foods"][0]["foodNutrients"][0])
 
+protein = None
+carbs = None
+fat = None
 for nutrients in data["foods"][0]["foodNutrients"]:
     if (nutrients["nutrientNumber"] in ["203", "204", "205"]):
-        print(f"The number is {nutrients['nutrientNumber']} and the name of the macro is {nutrients['nutrientName']}.")
-        print (f"The actual value for this item is {nutrients['value']} {nutrients['unitName']}")
+        if nutrients['nutrientNumber'] == "203":
+            protein = nutrients['value']
+        elif nutrients['nutrientNumber'] == "204":
+            fat = nutrients['value']
+        elif nutrients['nutrientNumber'] == "205":
+            carbs = nutrients['value']
+
+insert_food_log("2026-06-23", "chicken breast", protein, carbs, fat, 25, 3)
+
+#print(f"The number is {nutrients['nutrientNumber']} and the name of the macro is {nutrients['nutrientName']}.")
+#print (f"The actual value for this item is {nutrients['value']} {nutrients['unitName']}")
+
